98cf5f5d2493925b66fa4ed03c16f71f5d1b8ca7
DevTony101 <tonycortesano@gmail.com>
2020-05-27 13:23:26 -0500

    Adding new 404 page and redirecting all non existing urls to it

--- src/router/index.js
+++ src/router/index.js
@@ -16,15 +16,16 @@ const routes = [
     name: "Documentation",
     component: Documentation,
   },
-  /*{
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/!* webpackChunkName: "about" *!/ "../views/About.vue")
-  }*/
+  {
+    path: "/404",
+    name: "404",
+    props: true,
+    component: () => import("../views/NotFound.vue"),
+  },
+  {
+    path: "*",
+    redirect: { name: "404", params: { resource: "page" } },
+  },
 ];
 
 const router = new VueRouter({
---
+++ src/views/NotFound.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <img v-bind:src="source" alt="404" />
+    <h1>
+      It appears that the {{ resource }} that you are looking for is nowhere to
+      be found!
+    </h1>
+  </div>
+</template>
+
+<script>
+  import { mapState } from "vuex";
+
+  export default {
+    name: "NotFound",
+    mounted() {
+      this.fetchImage();
+    },
+    props: {
+      resource: {
+        type: String,
+        required: true,
+      },
+    },
+    data: function() {
+      return {
+        source: "",
+      };
+    },
+    methods: {
+      fetchImage: function() {
+        let accentColor = getComputedStyle(document.body)
+          .getPropertyValue("--color-accent")
+          .substr(3);
+        console.log("Thanks to blairlee227 for the image!");
+        this.source = `https://illustatus.herokuapp.com/?title=&fill=%23${accentColor}`;
+      },
+    },
+    computed: mapState(["theme"]),
+    watch: {
+      // eslint-disable-next-line no-unused-vars
+      theme: function(value, oldValue) {
+        this.fetchImage();
+      },
+    },
+  };
+</script>
+
+<style scoped>
+  div {
+    width: 80%;
+    margin: auto;
+  }
+
+  h1 {
+    text-align: center;
+  }
+</style>
