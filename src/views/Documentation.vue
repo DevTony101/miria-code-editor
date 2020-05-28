e9325a99cff7c38a26533e0e7e52cf9b1180ae2b
DevTony101 <tonycortesano@gmail.com>
2020-05-27 21:57:08 -0500

    Fixing typos in docs

--- src/views/Documentation.vue
+++ src/views/Documentation.vue
@@ -11,26 +11,26 @@
       declare the type of your variable before you can do anything with it, in
       fact, those who programmed in <b>Swift</b> will see a resemblance, however
       <b>Miria</b> natively only supports three datatypes: <i>Number</i>,
-      <i>String</i> and <i>Boolean</i>, this specs are explained thoroughly in
-      coming sections. There iss also a fourth datatype <i>Void</i> that stands
+      <i>String</i> and <i>Boolean</i>, these specs are explained thoroughly in
+      coming sections. There is also a fourth datatype <i>Void</i> that stands
       for the lack of a return statement in a function definition.
     </p>
     <p>
       Miria's syntax was made with effectiveness of code in mind, meaning that
-      the resulting code would be as self explanatory as possible, since it
+      the resulting code would be as self-explanatory as possible, since it
       picks up the best from other programming languages.
     </p>
     <h3>Getting Started</h3>
     <hr />
     <p>
       In a style similar to <b>C</b>, your source code must be enclosed in
-      <i>functions</i> and this functions must be called from within a single
+      <i>functions</i> and these functions must be called from within a single
       function <b>main</b> that returns, yeah you guessed it, <b>void</b>. The
-      reason behind this, at least for <b>Miria</b>, its because its so much
-      easy to parse than having to write every possible grammatical rule in a
-      <i>top level statement</i>. Of course you, the programmer, does not care
+      reason behind this, at least for <b>Miria</b>, it's because it's so much
+      easier to parse than having to write every possible grammatical rule in a
+      <i>top level statement</i>. Of course you, the programmer, do not care
       about this, your only concern is how to write a <i>Hello World</i> program
-      in <b>Miria</b>, and is as simple as:
+      in <b>Miria</b>, and it's as simple as:
     </p>
     <pre>
       define main as fun() -> void {
@@ -57,8 +57,8 @@
     <hr />
     <p>
       A programming language wouldn't be one if it didn't support the evaluation
-      of expressions of some sort (sorry HTML). But what are expressions? First
-      we must define what an <b>statement</b> is, an statement is just a
+      of expressions of some sort (sorry HTML). But, what are expressions?
+      First, we must define what an <b>statement</b> is, an statement is just a
       <i>command</i>, that your PC can recognize through a programming language,
       an <b>expression</b> is just an statement that returns a value, not
       necessarily a numeric one, currently <b>Miria</b> only supports the
@@ -85,7 +85,7 @@
     <p>
       Printing out numbers and boolean literals is cool and all, but most of the
       time you would want to store the result of an expression for later use,
-      that's when <b>variables</b> come in. Lets say you want to print a very
+      that's when <b>variables</b> come in. Let's say you want to print a very
       polite salute along with the name of the person, you may do something like
       this:
     </p>
@@ -116,7 +116,7 @@
       </li>
     </ul>
     <p>
-      Now lets say you want to say whether a number is even or not, you may do
+      Now let's say you want to say whether a number is even or not, you may do
       something like this:
     </p>
     <pre>
@@ -269,7 +269,7 @@
       }</pre
     >
     <p>
-      The <b>range</b> function take exactly two arguments and returns a list
+      The <b>range</b> function takes exactly two arguments and returns a list
       with values that goes from <b><i>a</i></b> to <b><i>b - 1</i></b
       >, if you pass just one argument, it will be considered as the upper limit
       and the values will go from <b><i>0</i></b> to <b><i>b - 1</i></b
